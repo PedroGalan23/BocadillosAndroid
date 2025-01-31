36841a9736629487c0b53062e6a037783b306b1b
PedroGalan23 <pedrogalanagu1@gmail.com>
2025-02-01 00:11:23 +0100

    Navigation Completado

--- app/src/main/java/com/example/bocadilloandroid/DashBoardAlumno.kt
+++ app/src/main/java/com/example/bocadilloandroid/DashBoardAlumno.kt
@@ -3,6 +3,7 @@ package com.example.bocadilloandroid
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -11,7 +12,9 @@ class DashBoardAlumno : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dashboard)
 
-        val navController = findNavController(R.id.nav_host_fragment) // Forma correcta en Activity
+        // Obtener el NavHostFragment correctamente
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNavigationView.setupWithNavController(navController)
